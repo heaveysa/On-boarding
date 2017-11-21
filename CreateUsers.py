21efc89dc1f8d4d0f859a248216fe08f4f9503ac
Sam Heavey <samheavey@MacBook-Pro-3.local>
2017-11-21 09:21:10 +0000

    Commit number one

---
+++ CreateUsers.py
@@ -0,0 +1,97 @@
+from __future__ import print_function
+import httplib2
+import os
+
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/admin-directory_v1-python-quickstart.json
+SCOPES = 'https://www.googleapis.com/auth/admin.directory.user'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Directory API Python Quickstart'
+
+
+def get_credentials():
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'admin-directory_v1-python-quickstart.json')
+
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+def main():
+    """Shows basic usage of the Google Admin SDK Directory API.
+
+    Creates a Google Admin SDK API service object and outputs a list of first
+    10 users in the domain.
+    """
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('admin', 'directory_v1', http=http)
+
+    g_name = 'test'
+    f_name = 'testy'
+
+    Fullname = {
+    'name.familyName':f_name,
+    'name.givenName':g_name
+    }
+
+    Email = g_name + '.' + f_name + '@websummit.com'
+
+    pEmail = {
+    'password' : "TheSummit2017",
+    'primaryEmail': Email,
+    'name': {'familyName':f_name, 'givenName':g_name}
+    }
+
+    PW = {
+        'password' : "TheSummit2017"
+    }
+
+    print(Email)
+    print('Getting the first 100 users in the domain')
+    results = service.users().insert(body = pEmail).execute()
+    #users = results.get('users', [])
+
+    if not users:
+        print('No users in the domain.')
+    else:
+        print('Users:')
+        for user in users:
+            print('{0} ({1})'.format(user['primaryEmail'],
+                user['name']['fullName']))
+
+
+if __name__ == '__main__':
+    main()
